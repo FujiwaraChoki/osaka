94fc937de395a304bd71b2e5a54ba30b624d903b
FujiwaraChoki <fuji@fuji.codes>
2024-06-08 13:26:33 +0200

    feat: debugging with bochs

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -77,6 +77,13 @@ main:
     mov ss, ax
     mov sp, 0x7C00
 
+    ; read something
+    mov [ebr_drive_number], dl
+    mov ax, 1
+    mov cl, 1
+    mov bx, 0x7E00
+    call disk_read
+
     ; print silly osaker message
     mov si, osaker_prefix
     call puts
@@ -92,14 +99,113 @@ main:
 
     hlt
 
+floppy_error:
+    mov si, read_failed
+    call puts
+    jmp wait_key_and_reboot
+
+wait_key_and_reboot:
+    mov ah, 0
+    int 16h
+    jmp 0FFFFh:0
+
 .halt:
-    jmp .halt
+    cli
+    hlt
+
+; disk routines (LBA, CSH)
+; converts lba addy to a chs addy
+; params:
+; - ax: lba
+; returns:
+; - cs [bits 0-5]: sector number
+; - cs [bits 6-15]: cylinder
+; - dh: head
+lba_to_chs:
+    push ax
+    push dx
+
+    xor dx, dx
+    div word [bdb_sectors_per_track]        ; ax = lba / SectorsPerTrack
+
+    inc dx
+    mov cx, dx
+    div word [bdb_heads]
+
+    mov dh, dl
+    mov ch, al
+    shl ah, 6
+    or cl, ah
+
+    pop ax
+    mov al, al
+    pop ax
+    ret
+
+; reads sectors from a disk
+; params:
+; - ax: lba addy
+; - cl: number of sectors to read
+; - dl: drive number
+; - es:bx: mem addy where to store read data
+
+disk_read:
+
+    push ax
+    push bx
+    push cx
+    push dx
+    push di
+
+    push cx         ; save cl
+    call lba_to_chs
+    pop ax
+    mov ah, 02h
+    mov di, 3       ; retry count (for safety)
+
+.retry:
+    pusha
+    stc     ; set carry flag (some don't make it)
+    int 13h
+
+    jnc .done
+    popa
+    call disk_reset
+
+    dec di
+    test di, di
+    jnz .retry
+
+.fail:
+    ; attempts exhausted
+    call floppy_error
+
+.done:
+    popa
+
+    push di
+    push dx
+    push cx
+    push bx
+    push ax
+    ret
+
+disk_reset:
+    pusha
+    mov ah, 0
+    stc
+    int 13h
+    jc floppy_error
+    popa
+    ret
 
 osaker_prefix: db 'Osaker says: ', 00
 chiyo_prefix: db 'Chiyo-Chan: ', 00
 
 osaker: db 'America Ya!', ENDL, 00
 chiyo: db 'Hallo!', ENDL, 00
+
+read_failed: db 'Failed to read from disk', ENDL, 00
 
 times 510-($-$$) db 0 ; length of program so far
 dw 0AA55h ; bios needs this
